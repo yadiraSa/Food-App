ab880863de2bf5730595122a836d375f66f5c95c
Yadira Salazar <yadira.salazar@clickittech.com.mx>
2022-08-18 13:35:47 -0500

    Handling Errors

--- food-order-app/01-starting-project/src/components/Meals/AvailableMeals.js
+++ food-order-app/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -10,13 +10,18 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [httpError, setHttpError] = useState();
 
   useEffect( () => {
     const fetchMeals = async () => {
     const response = await fetch('https://react-http-19124-default-rtdb.firebaseio.com/meals.json');
+    
+      if(!response.ok) {
+        throw new Error('Somethin went wrong!');
+      }
+
     const responseData = await response.json();
-  
+    
     const loadedMeals = [];
 
     for (const key in responseData) {
@@ -30,7 +35,16 @@ const AvailableMeals = () => {
     setMeals(loadedMeals);
     setIsLoading(false);
   };
-  fetchMeals();
+
+  try{
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  } catch (error) {
+    setIsLoading(false);
+    setHttpError(error.message);
+  }
   }, []);
 
   if(isLoading){
@@ -38,7 +52,13 @@ const AvailableMeals = () => {
       <section className={classes.MealsLoading} >
         <p>Loading...</p>
       </section>
-    )
+    );
+  }
+
+  if(httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
   }
 
     const mealsList = meals.map((meal) => (
